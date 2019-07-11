f06c88f41fe3c232053561256697afa04c4b687f
Daniel <ssejjembadan@gmail.com>
2019-07-11 20:23:53 +0300

    Added finding the second largest

--- src/Find.java
+++ src/Find.java
@@ -25,10 +25,34 @@ public class Find {
         return smallest;
     }
 
+    //Find the second largest number in the array.
+    public static int secondLargest(int[] arr){
+
+        boolean swapped;
+        //sort in descending order
+        do {
+            swapped = false;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i + 1] > arr[i]) {
+                    //swap
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+        }while (swapped);
+
+        return arr[1];
+
+    }
+
+
     public static void main(String[] args){
         int[] arr = {1,2,3,4,5,6,7,8,9};
         System.out.println("the largest of all the values in array:" + FindLargest(arr));
-        System.out.println("the smalles of all the values in array:" + FindSmallest(arr));
+        System.out.println("the smallest of all the values in array:" + FindSmallest(arr));
+        System.out.println("the second largest of all the values in array:" + secondLargest(arr));
     }
 
 }
