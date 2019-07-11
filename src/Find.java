74b824ffd1047c92af167f9a335db1b52a7d998c
Daniel <ssejjembadan@gmail.com>
2019-07-11 19:50:51 +0300

    Added finding smallest number

--- src/Find.java
+++ src/Find.java
@@ -1,6 +1,7 @@
-//Find the largest element in the array.
+
 public class Find {
 
+    //Find the largest element in the array.
     public static int FindLargest(int[] arr){
         int largest = Integer.MIN_VALUE;
 
@@ -12,9 +13,22 @@ public class Find {
         return largest;
     }
 
+    //Find the smallest element in the array.
+    public static int FindSmallest(int[] arr){
+        int smallest = Integer.MAX_VALUE;
+
+        for (int i1 : arr) {
+            if (i1 < smallest)
+                smallest = i1;
+        }
+
+        return smallest;
+    }
+
     public static void main(String[] args){
         int[] arr = {1,2,3,4,5,6,7,8,9};
         System.out.println("the largest of all the values in array:" + FindLargest(arr));
+        System.out.println("the smalles of all the values in array:" + FindSmallest(arr));
     }
 
 }
